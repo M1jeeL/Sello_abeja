6e34f0ee08d8a282ef5cf91d856227b85cfd5be5
Miguel Loza <miguelloza1@hotmail.com>
2022-10-06 16:21:50 -0300

    Agregadas validaciones a serializador user

--- selloabeja/api/serializers.py
+++ selloabeja/api/serializers.py
@@ -2,6 +2,7 @@
 #from django.contrib.auth.hashers import make_password
 from rest_framework import serializers, validators
 from .models import Address, Client, Commune, Region, User
+import re
 
 class RegionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -45,13 +46,40 @@ class UserSerializer(serializers.ModelSerializer):
                 }
             }
         }
+
     def validate_password(self, password):
+        if password == '':
+            raise serializers.ValidationError('La contraseña no puede estar en blanco')
+
         if len(password) < 7:
             raise serializers.ValidationError('La contraseña debe ser mayor a 7')
+
         if password.isnumeric():
             raise serializers.ValidationError('La contraseña no puede ser solo numerica')
         return password
 
+    def validate_email(self, email):
+        if email == '':
+            raise serializers.ValidateError('El email no puede estar en blanco')
+        return email
+
+    def validate_first_name(self, first_name):
+        if (not re.fullmatch(r"[A-Za-z ]{1,20}", first_name)):
+            raise serializers.ValidateError('El nombre solo puede contener letras')
+
+        if first_name == '':
+            raise serializers.ValidateError('El nombre no puede estar en blanco')
+
+        return first_name
+        
+    def validate_last_name(self, last_name):
+        if (not re.fullmatch(r"[A-Za-z ]{1,20}", last_name)):
+            raise serializers.ValidateError('El apellido solo puede contener letras')
+
+        if last_name == '':
+            raise serializers.ValidateError('El apellido no puede estar en blanco')
+
+        return last_name
 
 class ClientSerializer(serializers.ModelSerializer):
     client = UserSerializer(required=True)
